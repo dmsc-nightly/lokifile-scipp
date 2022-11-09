54ea67485a1d0975bf835472862bd8889b000777
Neil Vaytet <neil.vaytet@esss.se>
2022-11-09 14:04:18 +0100

    initial files

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone git@github.com:ess-dmsc/kafka-to-nexus.git
+cd kafka-to-nexus/
+conan config install http://github.com/ess-dmsc/conan-configuration.git
+mkdir _build
+cd _build
+conan install .. --build=missing
