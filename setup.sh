181bc5d18862915e1d8d573e3aaab5a8f88996ac
Neil Vaytet <neil.vaytet@esss.se>
2022-11-09 15:03:46 +0100

    try setting conan settings

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 git clone https://github.com/ess-dmsc/kafka-to-nexus.git
 cd kafka-to-nexus/
+conan config set settings.compiler=gcc
+conan config set settings.compiler.libcxx=libstdc++11
+conan config set settings.compiler.version=11
 conan config install http://github.com/ess-dmsc/conan-configuration.git
 mkdir _build
 cd _build
